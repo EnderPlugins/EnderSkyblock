a378c9f779eea4fb09a1255868a1744b2f78feca
YouHaveTrouble <garrenpolska@gmail.com>
2020-06-29 02:30:18 +0200

    swm import

--- enderskyblock-spigot/src/main/java/eu/endermite/skyblock/EnderSkyblockSpigot.java
+++ enderskyblock-spigot/src/main/java/eu/endermite/skyblock/EnderSkyblockSpigot.java
@@ -1,6 +1,8 @@
 package eu.endermite.skyblock;
 
+import com.grinderwolf.swm.api.SlimePlugin;
 import eu.endermite.skyblock.cache.ConfigCache;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EnderSkyblockSpigot extends JavaPlugin {
@@ -9,12 +11,16 @@ public class EnderSkyblockSpigot extends JavaPlugin {
     public static EnderSkyblockSpigot getPlugin() {return plugin;}
     private static ConfigCache configCache;
     public static ConfigCache getConfigCache() {return configCache;}
+    private static SlimePlugin swm;
+    public static SlimePlugin getSwm() {return swm;}
+
 
     @Override
     public void onEnable() {
 
         plugin = this;
         configCache = new ConfigCache();
+        swm = (SlimePlugin) Bukkit.getPluginManager().getPlugin("SlimeWorldManager");
 
         getLogger().info("Enabling EnderSkyblock...");
 
