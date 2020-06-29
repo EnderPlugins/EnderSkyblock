bcdd41cdd60b51a91ba4906b0a9ee10c4963b88e
YouHaveTrouble <garrenpolska@gmail.com>
2020-06-29 02:30:59 +0200

    basic island creation

--- enderskyblock-spigot/src/main/java/eu/endermite/skyblock/worldmanager/IslandCreate.java
+++ enderskyblock-spigot/src/main/java/eu/endermite/skyblock/worldmanager/IslandCreate.java
@@ -1,8 +1,17 @@
 package eu.endermite.skyblock.worldmanager;
 
+import com.grinderwolf.swm.api.SlimePlugin;
+import com.grinderwolf.swm.api.loaders.SlimeLoader;
+import com.grinderwolf.swm.api.world.SlimeWorld;
+import com.grinderwolf.swm.api.world.properties.SlimeProperties;
+import com.grinderwolf.swm.api.world.properties.SlimePropertyMap;
+import eu.endermite.skyblock.EnderSkyblockSpigot;
+
 import java.util.UUID;
 
 public class IslandCreate {
+
+    SlimePlugin swm = EnderSkyblockSpigot.getSwm();
 
 
     /**
@@ -11,9 +20,26 @@ public class IslandCreate {
      */
     public UUID createNewIsland(String schematic) {
 
-        // TODO island creation logic
+        SlimeLoader loader = swm.getLoader("mysql");
 
-        return null;
+        //TODO customisability support
+
+        SlimePropertyMap props = new SlimePropertyMap();
+        props.setString(SlimeProperties.DIFFICULTY, "normal");
+        props.setInt(SlimeProperties.SPAWN_X, 0);
+        props.setInt(SlimeProperties.SPAWN_Y, 0);
+        props.setBoolean(SlimeProperties.PVP, false);
+
+        try {
+            SlimeWorld world = swm.loadWorld(loader, schematic, true, props);
+            UUID island_uuid = UUID.randomUUID();
+            world.clone(island_uuid.toString(), loader);
+            return island_uuid;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
